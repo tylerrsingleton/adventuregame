fe4302811c0e83f57825b7fae8b6be53f52e30bf
tylerrsingleton <36518116+tylerrsingleton@users.noreply.github.com>
2018-02-15 16:01:51 -0800

    Add files via upload

---
+++ adventureGameProject.py
@@ -0,0 +1,30 @@
+### Adventure Game Project ###
+#By Friday, 2/16/18, create a window and charcter
+#that can move side to side with the arrow keys.
+
+#import and initialize pygame
+import pygame
+pygame.init()
+
+#Create window
+w = pygame.display.set_mode([1000,500])
+
+#Create character
+character = pygame.Rect(900,450,0,10,10)
+
+#Create main loop
+drawing = True
+while drawing:
+    #Create event loop
+    for event in pygame.event.get():
+        if event.type == pyamge.QUIT:
+            drawing = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character.x -= 5
+            elif event.key == pygame.k_DOWN:
+                character.y += 5
+
+    #Draw the character
+    pygame.draw.ellipse(0, (0,255,255), character)
+    
